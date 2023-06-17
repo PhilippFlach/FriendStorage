d0c0021b972fcf53ac4366167a779bff00c228f9
Philipp Flach <philipp.flach@luzi.com>
2023-06-17 23:30:18 +0200

    Addes MSUnit Test and created 1 unit test

--- ThomasMathLib/ThomasMathLib/Calculator.cs
+++ ThomasMathLib/ThomasMathLib/Calculator.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ThomasMathLib
 {
-    public class Class1
+    public class Calculator
     {
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
     }
 }
