aacb44b2ca39af55db2a32a57256a5f79bf70e04
Philipp Flach <philipp.flach@luzi.com>
2023-06-17 22:51:33 +0200

    Unit test with MSTest

---
+++ ThomasMathLib/ThomasMathLibTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThomasMathLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThomasMathLib.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddShouldReturnSumOfValues()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
